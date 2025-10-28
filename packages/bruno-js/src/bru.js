05891b33fcb1799a7ae6ee62d3866af636994042
Bipul <123935352+Syed-Bipul-Rahman@users.noreply.github.com>
2025-10-28 20:40:16 +0600

    feat: add getman-js package

---
+++ packages/bruno-js/src/bru.js
@@ -0,0 +1,282 @@
+const { cloneDeep } = require('lodash');
+const xmlFormat = require('xml-formatter');
+const { interpolate: _interpolate } = require('@usebruno/common');
+const { sendRequest } = require('@usebruno/requests').scripting;
+const { jar: createCookieJar } = require('@usebruno/requests').cookies;
+
+const variableNameRegex = /^[\w-.]*$/;
+
+class Bru {
+  constructor(envVariables, runtimeVariables, processEnvVars, collectionPath, collectionVariables, folderVariables, requestVariables, globalEnvironmentVariables, oauth2CredentialVariables, collectionName) {
+    this.envVariables = envVariables || {};
+    this.runtimeVariables = runtimeVariables || {};
+    this.processEnvVars = cloneDeep(processEnvVars || {});
+    this.collectionVariables = collectionVariables || {};
+    this.folderVariables = folderVariables || {};
+    this.requestVariables = requestVariables || {};
+    this.globalEnvironmentVariables = globalEnvironmentVariables || {};
+    this.oauth2CredentialVariables = oauth2CredentialVariables || {};
+    this.collectionPath = collectionPath;
+    this.collectionName = collectionName;
+    this.sendRequest = sendRequest;
+    this.cookies = {
+      jar: () => {
+        const cookieJar = createCookieJar();
+                
+        return {
+          getCookie: (url, cookieName, callback) => {
+            const interpolatedUrl = this.interpolate(url);
+            return cookieJar.getCookie(interpolatedUrl, cookieName, callback);
+          },
+
+          getCookies: (url, callback) => {
+            const interpolatedUrl = this.interpolate(url);
+            return cookieJar.getCookies(interpolatedUrl, callback);
+          },
+
+          setCookie: (url, nameOrCookieObj, valueOrCallback, maybeCallback) => {
+            const interpolatedUrl = this.interpolate(url);
+            return cookieJar.setCookie(interpolatedUrl, nameOrCookieObj, valueOrCallback, maybeCallback);
+          },
+
+          setCookies: (url, cookiesArray, callback) => {
+            const interpolatedUrl = this.interpolate(url);
+            return cookieJar.setCookies(interpolatedUrl, cookiesArray, callback);
+          },
+
+          // Clear entire cookie jar
+          clear: (callback) => {
+            return cookieJar.clear(callback);
+          },
+
+          // Delete cookies for a specific URL/domain
+          deleteCookies: (url, callback) => {
+            const interpolatedUrl = this.interpolate(url);
+            return cookieJar.deleteCookies(interpolatedUrl, callback);
+          },
+
+          deleteCookie: (url, cookieName, callback) => {
+            const interpolatedUrl = this.interpolate(url);
+            return cookieJar.deleteCookie(interpolatedUrl, cookieName, callback);
+          }
+        };
+      }
+    };
+    // Holds variables that are marked as persistent by scripts
+    this.persistentEnvVariables = {};
+    this.runner = {
+      skipRequest: () => {
+        this.skipRequest = true;
+      },
+      stopExecution: () => {
+        this.stopExecution = true;
+      },
+      setNextRequest: (nextRequest) => {
+        this.nextRequest = nextRequest;
+      }
+    };
+
+    this.utils = {
+      minifyJson: (json) => {
+        if (json === null || json === undefined) {
+          throw new Error('Failed to minify');
+        }
+
+        if (typeof json === 'object') {
+          try {
+            return JSON.stringify(json);
+          } catch (err) {
+            throw new Error(`Failed to minify: ${err?.message || err}`);
+          }
+        }
+
+        if (typeof json === 'string') {
+          const trimmed = json.trim();
+          if (trimmed === '') return trimmed;
+          try {
+            return JSON.stringify(JSON.parse(trimmed));
+          } catch (err) {
+            throw new Error(`Failed to minify: ${err?.message || err}`);
+          }
+        }
+
+        throw new TypeError('minifyJson expects a string or object');
+      },
+
+      minifyXml: (xml) => {
+        if (xml === null || xml === undefined) {
+          throw new Error('Failed to minify');
+        }
+
+        if (typeof xml === 'string') {
+          try {
+            return xmlFormat(xml, { collapseContent: false, indentation: '', lineSeparator: '' });
+          } catch (err) {
+            throw new Error(`Failed to minify: ${err?.message || err}`);
+          }
+        }
+
+        throw new TypeError('minifyXml expects a string');
+      }
+    };
+  }
+
+  interpolate = (strOrObj) => {
+    if (!strOrObj) return strOrObj;
+    const isObj = typeof strOrObj === 'object';
+    const strToInterpolate = isObj ? JSON.stringify(strOrObj) : strOrObj;
+
+    const combinedVars = {
+      ...this.globalEnvironmentVariables,
+      ...this.collectionVariables,
+      ...this.envVariables,
+      ...this.folderVariables,
+      ...this.requestVariables,
+      ...this.oauth2CredentialVariables,
+      ...this.runtimeVariables,
+      process: {
+        env: {
+          ...this.processEnvVars
+        }
+      }
+    };
+
+    const interpolatedStr = _interpolate(strToInterpolate, combinedVars);
+    return isObj ? JSON.parse(interpolatedStr) : interpolatedStr;
+  };
+
+  cwd() {
+    return this.collectionPath;
+  }
+
+  getEnvName() {
+    return this.envVariables.__name__;
+  }
+
+  getProcessEnv(key) {
+    return this.processEnvVars[key];
+  }
+
+  hasEnvVar(key) {
+    return Object.hasOwn(this.envVariables, key);
+  }
+
+  getEnvVar(key) {
+    return this.interpolate(this.envVariables[key]);
+  }
+
+  setEnvVar(key, value, options = {}) {
+    if (!key) {
+      throw new Error('Creating a env variable without specifying a name is not allowed.');
+    }
+
+    if (variableNameRegex.test(key) === false) {
+      throw new Error(
+        `Variable name: "${key}" contains invalid characters! Names must only contain alpha-numeric characters, "-", "_", "."`
+      );
+    }
+
+    // When persist is true, only string values are allowed
+    if (options?.persist && typeof value !== 'string') {
+      throw new Error(`Persistent environment variables must be strings. Received ${typeof value} for key "${key}".`);
+    }
+
+    this.envVariables[key] = value;
+
+    if (options?.persist) {
+      this.persistentEnvVariables[key] = value;
+    } else {
+      if (this.persistentEnvVariables[key]) {
+        delete this.persistentEnvVariables[key];
+      }
+    }
+  }
+
+  deleteEnvVar(key) {
+    delete this.envVariables[key];
+  }
+
+  getGlobalEnvVar(key) {
+    return this.interpolate(this.globalEnvironmentVariables[key]);
+  }
+
+  setGlobalEnvVar(key, value) {
+    if (!key) {
+      throw new Error('Creating a env variable without specifying a name is not allowed.');
+    }
+
+    this.globalEnvironmentVariables[key] = value;
+  }
+
+  getOauth2CredentialVar(key) {
+    return this.interpolate(this.oauth2CredentialVariables[key]);
+  }
+
+  hasVar(key) {
+    return Object.hasOwn(this.runtimeVariables, key);
+  }
+
+  setVar(key, value) {
+    if (!key) {
+      throw new Error('Creating a variable without specifying a name is not allowed.');
+    }
+
+    if (variableNameRegex.test(key) === false) {
+      throw new Error(
+        `Variable name: "${key}" contains invalid characters!` +
+          ' Names must only contain alpha-numeric characters, "-", "_", "."'
+      );
+    }
+
+    this.runtimeVariables[key] = value;
+  }
+
+  getVar(key) {
+    if (variableNameRegex.test(key) === false) {
+      throw new Error(
+        `Variable name: "${key}" contains invalid characters!` +
+          ' Names must only contain alpha-numeric characters, "-", "_", "."'
+      );
+    }
+
+    return this.interpolate(this.runtimeVariables[key]);
+  }
+
+  deleteVar(key) {
+    delete this.runtimeVariables[key];
+  }
+
+  deleteAllVars() {
+    for (let key in this.runtimeVariables) {
+      if (this.runtimeVariables.hasOwnProperty(key)) {
+        delete this.runtimeVariables[key];
+      }
+    }
+  }
+
+  getCollectionVar(key) {
+    return this.interpolate(this.collectionVariables[key]);
+  }
+
+  getFolderVar(key) {
+    return this.interpolate(this.folderVariables[key]);
+  }
+
+  getRequestVar(key) {
+    return this.interpolate(this.requestVariables[key]);
+  }
+
+  setNextRequest(nextRequest) {
+    this.nextRequest = nextRequest;
+  }
+
+  sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  getCollectionName() {
+    return this.collectionName;
+  }
+}
+
+module.exports = Bru;
---
+++ packages/bruno-js/src/runtime/script-runtime.js
@@ -0,0 +1,383 @@
+const { NodeVM } = require('@usebruno/vm2');
+const path = require('path');
+const http = require('http');
+const https = require('https');
+const stream = require('stream');
+const util = require('util');
+const zlib = require('zlib');
+const url = require('url');
+const punycode = require('punycode');
+const fs = require('fs');
+const { get } = require('lodash');
+const Bru = require('../bru');
+const BrunoRequest = require('../bruno-request');
+const BrunoResponse = require('../bruno-response');
+const { cleanJson } = require('../utils');
+const { createBruTestResultMethods } = require('../utils/results');
+const { runScriptInNodeVm } = require('../sandbox/node-vm');
+
+// Inbuilt Library Support
+const ajv = require('ajv');
+const addFormats = require('ajv-formats');
+const atob = require('atob');
+const btoa = require('btoa');
+const lodash = require('lodash');
+const moment = require('moment');
+const uuid = require('uuid');
+const nanoid = require('nanoid');
+const axios = require('axios');
+const fetch = require('node-fetch');
+const chai = require('chai');
+const CryptoJS = require('crypto-js');
+const NodeVault = require('node-vault');
+const xml2js = require('xml2js');
+const cheerio = require('cheerio');
+const tv4 = require('tv4');
+const { executeQuickJsVmAsync } = require('../sandbox/quickjs');
+
+class ScriptRuntime {
+  constructor(props) {
+    this.runtime = props?.runtime || 'vm2';
+  }
+
+  // This approach is getting out of hand
+  // Need to refactor this to use a single arg (object) instead of 7
+  async runRequestScript(
+    script,
+    request,
+    envVariables,
+    runtimeVariables,
+    collectionPath,
+    onConsoleLog,
+    processEnvVars,
+    scriptingConfig,
+    runRequestByItemPathname,
+    collectionName
+  ) {
+    const globalEnvironmentVariables = request?.globalEnvironmentVariables || {};
+    const oauth2CredentialVariables = request?.oauth2CredentialVariables || {};
+    const collectionVariables = request?.collectionVariables || {};
+    const folderVariables = request?.folderVariables || {};
+    const requestVariables = request?.requestVariables || {};
+    const assertionResults = request?.assertionResults || [];
+    const bru = new Bru(envVariables, runtimeVariables, processEnvVars, collectionPath, collectionVariables, folderVariables, requestVariables, globalEnvironmentVariables, oauth2CredentialVariables, collectionName);
+    const req = new BrunoRequest(request);
+    const allowScriptFilesystemAccess = get(scriptingConfig, 'filesystemAccess.allow', false);
+    const moduleWhitelist = get(scriptingConfig, 'moduleWhitelist', []);
+    const additionalContextRoots = get(scriptingConfig, 'additionalContextRoots', []);
+    const additionalContextRootsAbsolute = lodash
+      .chain(additionalContextRoots)
+      .map((acr) => (acr.startsWith('/') ? acr : path.join(collectionPath, acr)))
+      .value();
+
+    const whitelistedModules = {};
+
+    for (let module of moduleWhitelist) {
+      try {
+        whitelistedModules[module] = require(module);
+      } catch (e) {
+        // Ignore
+        console.warn(e);
+      }
+    }
+
+    // extend bru with result getter methods
+    const { __brunoTestResults, test } = createBruTestResultMethods(bru, assertionResults, chai);
+
+    const context = {
+      bru,
+      req,
+      test,
+      expect: chai.expect,
+      assert: chai.assert,
+      __brunoTestResults: __brunoTestResults
+    };
+
+    if (onConsoleLog && typeof onConsoleLog === 'function') {
+      const customLogger = (type) => {
+        return (...args) => {
+          onConsoleLog(type, cleanJson(args));
+        };
+      };
+      context.console = {
+        log: customLogger('log'),
+        debug: customLogger('debug'),
+        info: customLogger('info'),
+        warn: customLogger('warn'),
+        error: customLogger('error')
+      };
+    }
+
+    if (runRequestByItemPathname) {
+      context.bru.runRequest = runRequestByItemPathname;
+    }
+
+    if (this.runtime === 'nodevm') {
+      await runScriptInNodeVm({
+        script,
+        context,
+        collectionPath,
+        scriptingConfig
+      });
+
+      return {
+        request,
+        envVariables: cleanJson(envVariables),
+        runtimeVariables: cleanJson(runtimeVariables),
+        persistentEnvVariables: bru.persistentEnvVariables,
+        globalEnvironmentVariables: cleanJson(globalEnvironmentVariables),
+        results: cleanJson(__brunoTestResults.getResults()),
+        nextRequestName: bru.nextRequest,
+        skipRequest: bru.skipRequest,
+        stopExecution: bru.stopExecution
+      };
+    }
+
+    if (this.runtime === 'quickjs') {
+      await executeQuickJsVmAsync({
+        script: script,
+        context: context,
+        collectionPath
+      });
+
+      return {
+        request,
+        envVariables: cleanJson(envVariables),
+        runtimeVariables: cleanJson(runtimeVariables),
+        persistentEnvVariables: bru.persistentEnvVariables,
+        globalEnvironmentVariables: cleanJson(globalEnvironmentVariables),
+        results: cleanJson(__brunoTestResults.getResults()),
+        nextRequestName: bru.nextRequest,
+        skipRequest: bru.skipRequest,
+        stopExecution: bru.stopExecution
+      };
+    }
+
+    // default runtime is vm2
+    const vm = new NodeVM({
+      sandbox: context,
+      require: {
+        context: 'sandbox',
+        builtin: [ "*" ],
+        external: true,
+        root: [collectionPath, ...additionalContextRootsAbsolute],
+        mock: {
+          // node libs
+          path,
+          stream,
+          util,
+          url,
+          http,
+          https,
+          punycode,
+          zlib,
+          // 3rd party libs
+          ajv,
+          'ajv-formats': addFormats,
+          atob,
+          btoa,
+          lodash,
+          moment,
+          uuid,
+          nanoid,
+          axios,
+          chai,
+          'node-fetch': fetch,
+          'crypto-js': CryptoJS,
+          xml2js: xml2js,
+          cheerio,
+          tv4,
+          ...whitelistedModules,
+          fs: allowScriptFilesystemAccess ? fs : undefined,
+          'node-vault': NodeVault
+        }
+      }
+    });
+    const asyncVM = vm.run(`module.exports = async () => { ${script} }`, path.join(collectionPath, 'vm.js'));
+    await asyncVM();
+
+    return {
+      request,
+      envVariables: cleanJson(envVariables),
+      runtimeVariables: cleanJson(runtimeVariables),
+      persistentEnvVariables: bru.persistentEnvVariables,
+      globalEnvironmentVariables: cleanJson(globalEnvironmentVariables),
+      results: cleanJson(__brunoTestResults.getResults()),
+      nextRequestName: bru.nextRequest,
+      skipRequest: bru.skipRequest,
+      stopExecution: bru.stopExecution
+    };
+  }
+
+  async runResponseScript(
+    script,
+    request,
+    response,
+    envVariables,
+    runtimeVariables,
+    collectionPath,
+    onConsoleLog,
+    processEnvVars,
+    scriptingConfig,
+    runRequestByItemPathname,
+    collectionName
+  ) {
+    const globalEnvironmentVariables = request?.globalEnvironmentVariables || {};
+    const oauth2CredentialVariables = request?.oauth2CredentialVariables || {};
+    const collectionVariables = request?.collectionVariables || {};
+    const folderVariables = request?.folderVariables || {};
+    const requestVariables = request?.requestVariables || {};
+    const assertionResults = request?.assertionResults || [];
+    const bru = new Bru(envVariables, runtimeVariables, processEnvVars, collectionPath, collectionVariables, folderVariables, requestVariables, globalEnvironmentVariables, oauth2CredentialVariables, collectionName);
+    const req = new BrunoRequest(request);
+    const res = new BrunoResponse(response);
+    const allowScriptFilesystemAccess = get(scriptingConfig, 'filesystemAccess.allow', false);
+    const moduleWhitelist = get(scriptingConfig, 'moduleWhitelist', []);
+    const additionalContextRoots = get(scriptingConfig, 'additionalContextRoots', []);
+    const additionalContextRootsAbsolute = lodash
+      .chain(additionalContextRoots)
+      .map((acr) => (acr.startsWith('/') ? acr : path.join(collectionPath, acr)))
+      .value();
+
+    const whitelistedModules = {};
+
+    for (let module of moduleWhitelist) {
+      try {
+        whitelistedModules[module] = require(module);
+      } catch (e) {
+        // Ignore
+        console.warn(e);
+      }
+    }
+
+    // extend bru with result getter methods
+    const { __brunoTestResults, test } = createBruTestResultMethods(bru, assertionResults, chai);
+
+    const context = {
+      bru,
+      req,
+      res,
+      test,
+      expect: chai.expect,
+      assert: chai.assert,
+      __brunoTestResults: __brunoTestResults
+    };
+
+    if (onConsoleLog && typeof onConsoleLog === 'function') {
+      const customLogger = (type) => {
+        return (...args) => {
+          onConsoleLog(type, cleanJson(args));
+        };
+      };
+      context.console = {
+        log: customLogger('log'),
+        info: customLogger('info'),
+        warn: customLogger('warn'),
+        error: customLogger('error'),
+        debug: customLogger('debug')
+      };
+    }
+
+    if (runRequestByItemPathname) {
+      context.bru.runRequest = runRequestByItemPathname;
+    }
+
+    if (this.runtime === 'nodevm') {
+      await runScriptInNodeVm({
+        script,
+        context,
+        collectionPath,
+        scriptingConfig
+      });
+
+      return {
+        response,
+        envVariables: cleanJson(envVariables),
+        persistentEnvVariables: cleanJson(bru.persistentEnvVariables),
+        runtimeVariables: cleanJson(runtimeVariables),
+        globalEnvironmentVariables: cleanJson(globalEnvironmentVariables),
+        results: cleanJson(__brunoTestResults.getResults()),
+        nextRequestName: bru.nextRequest,
+        skipRequest: bru.skipRequest,
+        stopExecution: bru.stopExecution
+      };
+    }
+
+    if (this.runtime === 'quickjs') {
+      await executeQuickJsVmAsync({
+        script: script,
+        context: context,
+        collectionPath
+      });
+
+      return {
+        response,
+        envVariables: cleanJson(envVariables),
+        persistentEnvVariables: cleanJson(bru.persistentEnvVariables),
+        runtimeVariables: cleanJson(runtimeVariables),
+        globalEnvironmentVariables: cleanJson(globalEnvironmentVariables),
+        results: cleanJson(__brunoTestResults.getResults()),
+        nextRequestName: bru.nextRequest,
+        skipRequest: bru.skipRequest,
+        stopExecution: bru.stopExecution
+      };
+    }
+
+    // default runtime is vm2
+    const vm = new NodeVM({
+      sandbox: context,
+      require: {
+        context: 'sandbox',
+        builtin: [ "*" ],
+        external: true,
+        root: [collectionPath, ...additionalContextRootsAbsolute],
+        mock: {
+          // node libs
+          path,
+          stream,
+          util,
+          url,
+          http,
+          https,
+          punycode,
+          zlib,
+          // 3rd party libs
+          ajv,
+          'ajv-formats': addFormats,
+          atob,
+          btoa,
+          lodash,
+          moment,
+          uuid,
+          nanoid,
+          axios,
+          'node-fetch': fetch,
+          'crypto-js': CryptoJS,
+          'xml2js': xml2js,
+          cheerio,
+          tv4,
+          ...whitelistedModules,
+          fs: allowScriptFilesystemAccess ? fs : undefined,
+          'node-vault': NodeVault
+        }
+      }
+    });
+
+    const asyncVM = vm.run(`module.exports = async () => { ${script} }`, path.join(collectionPath, 'vm.js'));
+    await asyncVM();
+
+    return {
+      response,
+      envVariables: cleanJson(envVariables),
+      persistentEnvVariables: cleanJson(bru.persistentEnvVariables),
+      runtimeVariables: cleanJson(runtimeVariables),
+      globalEnvironmentVariables: cleanJson(globalEnvironmentVariables),
+      results: cleanJson(__brunoTestResults.getResults()),
+      nextRequestName: bru.nextRequest,
+      skipRequest: bru.skipRequest,
+      stopExecution: bru.stopExecution
+    };
+  }
+}
+
+module.exports = ScriptRuntime;
